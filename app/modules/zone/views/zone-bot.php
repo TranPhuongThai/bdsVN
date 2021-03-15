2ebfef4aa2ed9b600656bf5c7948ac15bcfe76d7
Thai KV <phuongthai228@gmail.com>
2021-03-16 01:01:59 +0700

    thai

--- app/modules/zone/views/zone-bot.php
+++ app/modules/zone/views/zone-bot.php
@@ -1,52 +1,86 @@
 
 <div id="footer">
     <div class="container-fluid">
-        <div class="col-md-12 footer-content">
-            <div class="col-md-3 margin-top logo-bot">
-                <a href="#" class="logo" href="<?php echo base_url();?>" title="Đất vàng bình dương"><img width="100%" src="/public/template-tiendv/image/datvang.png" title="Đất vàng bình dương" alt="Đất vàng bình dương"/></a>
-            </div>
-            <div class="col-md-2">
-                <div class="row"><a href="#" class="link-bottom first-link-bottom"><b>Bất động sản TPHCM</b></a></div>
-                <div class="row"><a href="#" class="link-bottom">Mua bán nhà đất TPHCM</a></div>
-                <div class="row"><a href="#" class="link-bottom">Bán nhà quận 7</a></div>
-                <div class="row"><a href="#" class="link-bottom">Căn hộ quận 7</a></div>
-                <div class="row"><a href="#" class="link-bottom">Bán đất quận 9</a></div>
-                <div class="row"><a href="#" class="link-bottom">Phòng trọ quận 10</a></div>
-                <div class="row"><a href="#" class="link-bottom">Cho thuê nhà quận 8</a></div>
-                
-            </div>
-            <div class="col-md-2">
-                <div class="row"><a href="#" class="link-bottom first-link-bottom"><b>Bất động sản Hà Nội</b></a></div>
-                <div class="row"><a href="#" class="link-bottom">Bán đất Hà Nội</a></div>
-                <div class="row"><a href="#" class="link-bottom">Bán nhà Hà Nội</a></div>
-                <div class="row"><a href="#" class="link-bottom">Cho thuê nhà Hà Nội</a></div>
-                <div class="row"><a href="#" class="link-bottom">Chung cư Hà Nội</a></div>
-                <div class="row"><a href="#" class="link-bottom">Nhà đất Hà Nội</a></div>
-                <div class="row"><a href="#" class="link-bottom">Phòng trọ Hà Nội</a></div>
-            </div>
-            <div class="col-md-2">
-                <div class="row"><a href="#" class="link-bottom first-link-bottom"><b>Mua bán bất động sản</b></a></div>
-                <div class="row"><a href="#" class="link-bottom">Bán đất bình dương</a></div>
-                <div class="row"><a href="#" class="link-bottom">Bất động sản Đà Nẵng</a></div>
-                <div class="row"><a href="#" class="link-bottom">Căn hộ chung cư</a></div>
-                <div class="row"><a href="#" class="link-bottom">Mua bán nhà đất</a></div>
-                <div class="row"><a href="#" class="link-bottom">Nhà đất Hải Phòng</a></div>
-                <div class="row"><a href="#" class="link-bottom">Sang quán cafe</a></div>
-            </div>
-            <div class="col-md-2">
-                <div class="row"><a href="#" class="link-bottom first-link-bottom"><b> Cho thuê nhà đất </b></a></div>
-                <div class="row"><a href="#" class="link-bottom">Cho thuê căn hộ</a></div>
-                <div class="row"><a href="#" class="link-bottom">Cho thuê mặt bằng</a></div>
-                <div class="row"><a href="#" class="link-bottom">Cho thuê nhà nguyên căn</a></div>
-                <div class="row"><a href="#" class="link-bottom">Cho thuê nhà xưởng</a></div>
-                <div class="row"><a href="#" class="link-bottom">Cho thuê phòng trọ</a></div>
-                <div class="row"><a href="#" class="link-bottom">Cho thuê văn phòng</a></div>
+        <div class="row">
+            <div class="col-md-12 footer-content">
+                <div class="col-md-3 margin-top logo-bot">
+                    <a href="#" class="logo" href="<?php echo base_url();?>" title="Đất vàng bình dương"><img width="100%" src="/public/template-tiendv/image/datvang.png" title="Đất vàng bình dương" alt="Đất vàng bình dương"/></a>
+                    <div class="row text-center margin-top">
+                        Công ty cổ phần PropertyGuru Việt Nam
+                    </div>
+                    <div class="row text-center blue-text">
+                        Một thành viên của tập đoàn PropertyGuru
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="row"><a href="#" class="link-bottom first-link-bottom"><b>Bất động sản TPHCM</b></a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Mua bán nhà đất TPHCM</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Bán nhà quận 7</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Căn hộ quận 7</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Bán đất quận 9</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Phòng trọ quận 10</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Cho thuê nhà quận 8</a></div>
+                    </br>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom first-link-bottom"><b>Về Mogi</b></a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Về chúng tôi</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Chính sách bảo mật</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Điều khoản thỏa thuận</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Quy chế hoạt động</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Góc báo chí</a></div>
+                </div>
+                <div class="col-md-2">
+                    <div class="row"><a href="#" class="link-bottom first-link-bottom"><b>Bất động sản Hà Nội</b></a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Bán đất Hà Nội</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Bán nhà Hà Nội</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Cho thuê nhà Hà Nội</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Chung cư Hà Nội</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Nhà đất Hà Nội</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Phòng trọ Hà Nội</a></div>
+                    </br>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom first-link-bottom"><b>Đối tác - Thông tin</b></a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Tin tức bất động sản</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Muaban.net - Mua bán rao vặt</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Tìm việc làm thêm nhanh</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">TopCV.vn</a></div>
+                </div>
+                <div class="col-md-2">
+                    <div class="row"><a href="#" class="link-bottom first-link-bottom"><b>Mua bán bất động sản</b></a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Bán đất bình dương</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Bất động sản Đà Nẵng</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Căn hộ chung cư</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Mua bán nhà đất</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Nhà đất Hải Phòng</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Sang quán cafe</a></div>
+                    </br>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom first-link-bottom"><b>Công cụ - Tiện ích</b></a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Phòng trọ gần KCN</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Phòng trọ gần trường</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">10 bước mua nhà</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Môi giới bất động sản TPHCM</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Môi giới nhà đất Hà Nội</a></div>
+                </div>
+                <div class="col-md-2">
+                    <div class="row"><a href="#" class="link-bottom first-link-bottom"><b> Cho thuê nhà đất </b></a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Cho thuê căn hộ</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Cho thuê mặt bằng</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Cho thuê nhà nguyên căn</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Cho thuê nhà xưởng</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Cho thuê phòng trọ</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Cho thuê văn phòng</a></div>
+                    </br>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom first-link-bottom"><b>Dịch vụ - Quảng cáo</b></a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Chương trình - Khuyến mãi</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Phí thành viên</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Phí dịch vụ TOP/UP</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Hướng dẫn đăng tin</a></div>
+                    <div class="row row-link-bottom"><a href="#" class="link-bottom">Hướng dẫn thanh toán</a></div>
+                </div>
             </div>
         </div>
         <div class="clearfix"></div>
         <div class="margin-top hr-info"></div>
-        <div class="web-info text-left">
-            Thiết kế website: <a href="http://webbox.com.vn" target="_blank" title="Thiết kế website">Webbox</a>
+        <div class="web-info text-center">
+            <div class="row">Copy right @ 2021 <div class="bottom-text blue-text">- Bản quyền thuộc website bất động sản ABC - Vui lòng ghi rõ thông tin khi phát hành lại tin đăng</div></div>
             <div class="g-plus"><div class="g-plusone" data-href="<?php echo base_url();?>" data-size="medium"></div></div>
             <div class="fb-like" data-href="<?php echo base_url();?>" data-share="false" data-send="true" data-layout="button_count" data-width="120" data-show-faces="true"></div>
             <div class="clearfix"></div>
